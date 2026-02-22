a15f69674ad486597c77a3ec7ddf5283b50f1508
Ishfaq-scl <ishfaq@superiorcodelabs.com>
2026-02-22 11:42:39 +0530

    minor bug fix

--- index.js
+++ index.js
@@ -32,9 +32,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   const path = require("path");
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+
+    app.use((req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
 }
 
 app.get("/", (req, res) => {
